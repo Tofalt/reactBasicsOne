dda340afc9d8e2d4869b6a179658cc623e967efd
THOMAS FABULET <thomas.fabulet@gmail.com>
2026-06-08 22:21:10 +0200

    add pokemonlist in pokemonCard.tsx

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,7 +1,21 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 function Pokemoncard() {
+	const pokemon = pokemonList[1];
 	return <figure>
-		    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"/>
-            <figcaption>Bulbasaur</figcaption>
-	        </figure>;
+		{pokemon.imgSrc !== undefined ? (
+		    <img src={pokemon.imgSrc} alt={pokemon.name}/>):(
+				<p>???</p>
+			)}
+        <figcaption>{pokemon.name}</figcaption>
+	    </figure>;
 }
 export default Pokemoncard;
